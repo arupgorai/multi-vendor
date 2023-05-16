17434d62c83c25d8bacc3d551ddcb158f99deb0e
Arup Gorai 😎 <arup.gorai@digital.datamatics.com>
2023-05-16 21:56:38 +0530

    sign-up UI done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { LoginPage } from '../src/Routes';
+import { LoginPage, SignupPage } from '../src/Routes';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
